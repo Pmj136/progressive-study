7a392557ddfc4977e130a1c69a2f86e95bd3829c
PengMingJiu <pengmingjiu@gmail.com>
2022-04-27 17:09:26 +0800

    添加loading

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import AppAside from "./AppAside";
 import BreadCrumb from "./BreadCrumb";
+import Loading from '../components/icons/Loading'
+
 function Layout() {
     return (
         <div className="flex h-screen overflow-hidden">
@@ -9,7 +11,7 @@ function Layout() {
             <div className="flex flex-col flex-1 bg-gray-200">
                 <BreadCrumb />
                 <div className="flex-1 m-4 p-2 bg-white drop-shadow-sm transition-all">
-                    <Suspense fallback={<div>loading</div>}>
+                    <Suspense fallback={<Loading />}>
                         <Outlet />
                     </Suspense>
                 </div>
